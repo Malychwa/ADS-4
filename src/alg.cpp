7b6b283ec96ddb7b2977dcd64b975f9fc2863c4b
Малышева Галина <99182939+Malychwa@users.noreply.github.com>
2022-03-12 15:20:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,27 +24,34 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 
-int search(int* arr, int left, int right, int value) {
-  if (left <= right) {
-    int mind = left + (right - left) / 2;
-    if (arr[mind] == value) {
-      int l = search(arr, left, mind - 1, value);
-      int r = search(arr, mind + 1, right, value);
-      return l + r + 1;
-    }
-    if (arr[mind] > value) {
-      return search(arr, left, mind - 1, value);
-    }
-    return search(arr, mind + 1, right, value);
-  }
-  return 0;
-}
-
 
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len-1; i++) {
-    count += search(&arr[i + 1], 0, len - i, value - arr[i]);
+    int left = i;
+    int right = len - 1;
+    while (left < right) {
+      int mind = (left + right) / 2;
+      if (arr[i] + arr[mind] == value) {
+        count++;
+        int m = mind - 1;
+        while ((arr[i] + arr[m] == value) && (m > left)) {
+          count++;
+          m--;
+        }
+        m = mind + 1;
+        while ((arr[i] + arr[m] == value) && (m <= right)) {
+          count++;
+          m++;
+        }
+        break;
+      }
+      if (arr[i] + arr[mind] > value) {
+        right = mind;
+      } else {
+        left = mind;
+      }
+    }
   }
   return count;
 }
